d65beb0b6efa0ea282f2f35932648d3bb402a422
kennedymakokha <katchibo2@gmail.com>
2025-06-10 12:32:04 +0300

    fixing the  image  preview

--- src/server.ts
+++ src/server.ts
@@ -46,7 +46,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use('/api/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 // Routes
 
   connectDB();
